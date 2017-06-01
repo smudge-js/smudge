79f90490fdfbd65c5b18cfc05de7569c01c59fda
Justin Bakse <justin@tsfim.com>
2017-06-01 12:35:11 -0400

    clean up

--- notes/notes.md
+++ notes/notes.md
@@ -15,6 +15,9 @@ https://www.typescriptlang.org/docs/handbook/basic-types.htm
 lhttps://github.com/frontdevops/typescript-cheat-sheet
 https://www.npmjs.com/package/webpack-glsl-loader
 https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Getting_started_with_WebGL
-
-# Read
 https://www.typescriptlang.org/docs/handbook/interfaces.html
+https://www.opengl.org/discussion_boards/showthread.php/148444-Pixel-perfect-drawing
+http://glmatrix.net/docs/mat4.html
+http://typescript.codeplex.com/sourcecontrol/latest#typings/lib.d.ts
+https://basarat.gitbooks.io/typescript/content/docs/getting-started.html
+https://www.gitbook.com/
--- src/app.ts
+++ src/app.ts
@@ -11,128 +11,110 @@ helloGL();
 
 
 
+
+
 function helloGL(): void {
+    ///////////////////////////////////////////////////////////////
+    // GENERAL SETUP
 
-    // setup context
-    let canvas: HTMLCanvasElement = document.getElementById("gl-canvas") as HTMLCanvasElement;
-    let gl: WebGLRenderingContext = initWebGL(canvas);  //creates a WebGL context
+    // get context
+    let canvas = document.getElementById("gl-canvas") as HTMLCanvasElement;
+    let gl = initWebGL(canvas);
+    
+    // configure context
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.disable(gl.DEPTH_TEST);
+    
+    // create projection matrix
+    let pMatrix = mat4.create();
+    mat4.perspective(pMatrix, 45, canvas.width / canvas.height, 0.1, 100.0);
+    
+    // create model view matrix
+    let mvMatrix = mat4.create();
+    mat4.identity(mvMatrix);
 
-
-    // best practice for typescript? label every var?
+    // build shader program
     let vertSource = require("./glsl/basic_vertex.glsl");
     let fragSource = require("./glsl/basic_fragment.glsl");
-
     let shaderProgram: WebGLProgram = buildGLProgram(gl, vertSource, fragSource);
-    // build shaders
 
-
-    // if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    //     alert("Could not initialise shaders");
-    // }
-    console.log("shaderProgram", gl.getProgramParameter(shaderProgram, gl.LINK_STATUS));
-
+    // configure shader program
     gl.useProgram(shaderProgram);
     let vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-    // tell webgl we will provide aVertexPosition via an array
     gl.enableVertexAttribArray(vertexPositionAttribute);
-
-
-    // init vertex buffer
-    let squareVerticesBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer); //start working with
-
-    var vertices = [
-        2.0, 2.0, 0.0,
-        -1.0, 1.0, 0.0,
-        1.0, -1.0, 0.0,
-        -1.0, -1.0, 0.0,
-        -1.5, 0.0, 0.0
-    ];
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-
-
-
-
-
-
-    console.log(canvas.width);
-
-    gl.viewport(0, 0, canvas.width, canvas.height);
-
-
-    // clear background
-    gl.clearColor(0.0, 0.7, 0.0, 1.0);  //when we clear...
-    gl.enable(gl.DEPTH_TEST);  //use the depth buffer and test things against it //how far from the camera --> greyscale image (not linear, more density up front)
-    gl.depthFunc(gl.LEQUAL);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); //bitwise or -- optimized :)
-
-
-    // set up Projection Matrix
-    var pMatrix = mat4.create();
-    mat4.perspective(pMatrix, 45, canvas.width / canvas.height, 0.1, 100.0);
-    console.log("pMatrix", pMatrix);
-
-    // set up ModelView Matrix
-    var mvMatrix = mat4.create();
-    mat4.identity(mvMatrix);
-    mat4.translate(mvMatrix, mvMatrix, [0.0, 0.0, -7.0]);
-    console.log("mvMatrix", mvMatrix);
-
-    // draw
-    gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
-
-    //let vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-    gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-
     let pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
     let mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+
+    // build geo
+    let unitSquare = buildUnitSquare(gl);
+
+
+
+    ///////////////////////////////////////////////////////////////
+    // DRAW FRAME
+
+    // clear background
+    gl.clearColor(0.0, 0.7, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT); 
+
+    // set camera/cursor position
+    mat4.identity(mvMatrix);
+    mat4.translate(mvMatrix, mvMatrix, [0.0, 0.0, -7.0]);
+    
+    // draw square
+    gl.useProgram(shaderProgram);
+    gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
     gl.uniformMatrix4fv(pMatrixUniform, false, pMatrix);
     gl.uniformMatrix4fv(mvMatrixUniform, false, mvMatrix);
-
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 5);
+    
+    gl.bindBuffer(gl.ARRAY_BUFFER, unitSquare);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
 }
 
 
 
 
-function buildGLProgram(gl: WebGLRenderingContext, vertexSource: string, fragmentSource: string): WebGLProgram {
-    // var basicVertexShader = require(vertexPath); //projects 3d positions into screen space
-    // var basicFragmentShader = require(fragmentPath);  //strings
 
+function initWebGL(canvas: HTMLCanvasElement): WebGLRenderingContext {
+    let gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+    console.log("gl", !!gl);
+    return gl;
+}
+
+function buildGLProgram(gl: WebGLRenderingContext, vertexSource: string, fragmentSource: string): WebGLProgram {
     let vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexSource);
     gl.compileShader(vertexShader);
+    console.log("vertexShader", gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS));
 
     let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentSource);
     gl.compileShader(fragmentShader);
-
-    console.log("vertexShader", gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS));
     console.log("fragmentShader", gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS));
 
     var shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
+    console.log("shaderProgram", gl.getProgramParameter(shaderProgram, gl.LINK_STATUS));
 
     return shaderProgram;
 }
 
-function initWebGL(canvas: HTMLCanvasElement): WebGLRenderingContext {
-    let gl: WebGLRenderingContext = null;
 
-    // Try to grab the standard context. If it fails, fallback to experimental.
-    gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+function buildUnitSquare(gl: WebGLRenderingContext): WebGLBuffer {
 
-    // If we don't have a GL context, give up now
-    if (!gl) {
-        console.error('Unable to initialize WebGL. Your browser may not support it.');
-    }
+    var vertices = [
+        1.0, 1.0, 0.0,
+        -1.0, 1.0, 0.0,
+        1.0, -1.0, 0.0,
+        -1.0, -1.0, 0.0,
+    ];
 
-    return gl;
+    let buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer); 
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    return buffer;
 }
-
-// https://www.npmjs.com/package/webpack-glsl-loader
