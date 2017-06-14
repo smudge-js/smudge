e7170edca4d98890ce586203748f47a2d9535c58
Talia Cotton <taliacotton@gmail.com>
2017-06-14 02:18:12 -0400

    bricks blog post + photos

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import PBR from './js/pbr1';
-import draw from './js/sketches/brick_complex2'
+import draw from './js/sketches/brick_complex1'
 
-var pbr = new PBR(undefined, 1024, 1024, 4);
+var pbr = new PBR(undefined, 512, 512, 8);
 
 draw(pbr);
 
