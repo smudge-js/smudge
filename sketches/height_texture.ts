db78341e8f9c241206e5ce0e74c8a9f1acae61fc
Justin Bakse <justin@tsfim.com>
2018-04-14 20:01:47 -0400

    adding height_texture.ts test

---
+++ sketches/height_texture.ts
@@ -0,0 +1,27 @@
+import { Material2, Smudge, SmudgeUI } from '../src/js/index';
+
+export async function draw() {
+    // create a smudge instance
+    const smudge = new Smudge(undefined, 512, 512);
+
+    const clouds = await smudge.loadTexture("images/clouds.png");
+
+    // show the ui
+    const ui = new SmudgeUI(smudge);
+
+    // clear the drawing
+    smudge.clear();
+
+    // draw a rect
+    const testMaterial = new Material2();
+    testMaterial.albedo.color = .8;
+    testMaterial.height.textureInfo.texture = clouds;
+    testMaterial.height.color = .1;
+    smudge.rect(0, 0, 512, 512, testMaterial);
+
+    // show albedo in ui
+    ui.update2D();
+    ui.update3D();
+}
+
+draw();
