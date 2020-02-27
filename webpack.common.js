70dc47687b6fa5148e55025e066a562cbf3d5362
Justin Bakse <justin@tsfim.com>
2020-02-27 16:31:26 -0500

    split webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,1 +1,61 @@
-touch;
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const rules = [
+  {
+    test: /\.js$/,
+    enforce: 'pre',
+    loader: 'source-map-loader',
+  },
+  {
+    test: /\.ts$/,
+    enforce: 'pre',
+    use: 'source-map-loader',
+  },
+  {
+    test: /\.ts$/,
+    loader: 'ts-loader',
+    exclude: /node_modules/,
+  },
+  {
+    test: /\.(glsl|frag|vert)$/,
+    loader: 'raw-loader',
+    exclude: /node_modules/,
+  },
+  {
+    test: /\.(glsl|frag|vert)$/,
+    loader: 'glslify-loader',
+    exclude: /node_modules/,
+  },
+  {
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader'],
+  },
+  {
+    test: /\.(png|jpg)$/,
+    loader: 'url-loader',
+  },
+  {
+    test: /\.(html)$/,
+    loader: 'html-loader',
+  },
+];
+
+module.exports = {
+  entry: {
+    smudge: './src/js/smudge-p5',
+  },
+  output: {
+    filename: 'smudge-p5.js',
+    libraryTarget: 'var', // expose as variable
+    library: 'smudge_p5', // name the variable 'smudge_p5'
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'], // order to resolve extensions
+  },
+  module: {
+    rules,
+  },
+  plugins: [new CleanWebpackPlugin()],
+};
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,1 +1,7 @@
-touch;
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+});
